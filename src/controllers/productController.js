968dab6ce6a6cc3a35f124e3d81226754379a316
ArthurBortoleto <arthurmarcelbdossantos@gmail.com>
2024-09-20 08:59:09 -0300

    Criação e busca de produto

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,4 +1,4 @@
-import { getAllProducts } from '../services/productService.js';
+import { createProduct, getAllProducts, getProductById } from '../services/productService.js';
 
 export const getProducts = async (req, res) => {
     try{
@@ -7,7 +7,33 @@ export const getProducts = async (req, res) => {
     }
     catch (error) {
         res.state(500).json({
-            error: "Erro ao buscar produtos",
+            error: "Erro ao buscar produtos"
         });
+    }
+};
+
+export const createProductController = async (req, res) => {
+    try{
+        const newProduct = await createProduct(req.body);
+        res.status(201).json(newProduct)
+    }
+    catch (error) {
+        res.status(500).json({
+            error: "Erro ao criar produto"
+        });
+    }
+};
+
+export const getProduct = async(req, res) => {
+    try{
+        const product = await getProductById(req.params.id)
+        if(product){
+            res.json(product);
+        }else{
+            res.status(500).json({error:"Produto não encontrado"})
+        }
+    }
+    catch(error){
+        res.status(500).json({error:"Erro ao buscar produto"})
     }
 }
